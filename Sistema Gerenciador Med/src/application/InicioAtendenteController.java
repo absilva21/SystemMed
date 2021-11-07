8e5234957e6ae3057919e87276ac23b4d153ff23
alisson <alisson@alisson-All-Series>
2021-11-07 11:35:39 -0300

    Ajustes

---
+++ Sistema Gerenciador Med/src/application/InicioAtendenteController.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+public class InicioAtendenteController {
+	@FXML
+	void prontuOnAction(ActionEvent event) throws IOException {
+		BorderPane form = (BorderPane)FXMLLoader.load(getClass().getResource("listaProntuarios.fxml"));
+		Scene sceneform = new Scene(form,800,600);
+    	Main.aux.setScene(sceneform);
+	}
+	
+	@FXML
+	void voltarHome(ActionEvent event) throws IOException {
+		AnchorPane form = (AnchorPane)FXMLLoader.load(getClass().getResource("inicioAtendente.fxml"));
+		Scene sceneform = new Scene(form,800,600);
+    	Main.aux.setScene(sceneform);
+	}
+	
+	@FXML
+	void onMouseSair(ActionEvent event)throws IOException{
+		VBox form = (VBox)FXMLLoader.load(getClass().getResource("login.fxml"));
+    	Scene sceneform = new Scene(form,800,600);
+    	Main.aux.setScene(sceneform);
+	}
+	
+}
