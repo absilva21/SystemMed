51450b6118692ff697955b04bf4f8e17cf58221d
alisson <alisson@alisson-All-Series>
2021-11-15 19:58:37 -0300

    Update model

--- Sistema Gerenciador Med/src/model/Medico.java
+++ Sistema Gerenciador Med/src/model/Medico.java
@@ -4,7 +4,58 @@ public class Medico extends Pessoa{
 	String registro;
 	String orgaoRegistrador;
 	Agenda agenda;
+	Especialidade especialidade;
+	Especialidade subEspecialidade;
+	String senha;
 	
+	public String getRegistro() {
+		return registro;
+	}
+
+	public void setRegistro(String registro) {
+		this.registro = registro;
+	}
+
+	public String getOrgaoRegistrador() {
+		return orgaoRegistrador;
+	}
+
+	public void setOrgaoRegistrador(String orgaoRegistrador) {
+		this.orgaoRegistrador = orgaoRegistrador;
+	}
+
+	public Agenda getAgenda() {
+		return agenda;
+	}
+
+	public void setAgenda(Agenda agenda) {
+		this.agenda = agenda;
+	}
+
+	public Especialidade getEspecialidade() {
+		return especialidade;
+	}
+
+	public void setEspecialidade(Especialidade especialidade) {
+		this.especialidade = especialidade;
+	}
+
+	public Especialidade getSubEspecialidade() {
+		return subEspecialidade;
+	}
+
+	public void setSubEspecialidade(Especialidade subEspecialidade) {
+		this.subEspecialidade = subEspecialidade;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
 	public Medico() {
 		super();
 	}
