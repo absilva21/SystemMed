405a6f8933a3b7f585484da7d39d8643681044a3
alisson <alisson@alisson-All-Series>
2021-11-18 17:49:46 -0300

    get's e set's MainModel

--- Sistema Gerenciador Med/src/model/MainModel.java
+++ Sistema Gerenciador Med/src/model/MainModel.java
@@ -4,5 +4,23 @@ public class MainModel {
 	LinkList<Medico> medicos;
 	LinkList<Especialidade> especialidades;
 	Sessao sessao;
+	public LinkList<Medico> getMedicos() {
+		return medicos;
+	}
+	public void setMedicos(LinkList<Medico> medicos) {
+		this.medicos = medicos;
+	}
+	public LinkList<Especialidade> getEspecialidades() {
+		return especialidades;
+	}
+	public void setEspecialidades(LinkList<Especialidade> especialidades) {
+		this.especialidades = especialidades;
+	}
+	public Sessao getSessao() {
+		return sessao;
+	}
+	public void setSessao(Sessao sessao) {
+		this.sessao = sessao;
+	}
 
 }
