326d282376c8db1333cf1c8ec39b72737df7b1ca
alisson <alisson@alisson-All-Series>
2021-11-19 15:33:40 -0300

    update view

---
+++ Sistema Gerenciador Med/src/application/ListViewMedico.java
@@ -0,0 +1,9 @@
+package application;
+
+import javafx.scene.control.ListView;
+import model.Medico;
+
+
+public class ListViewMedico extends ListView<Medico> {
+
+}
---
+++ Sistema Gerenciador Med/src/application/ListaMedicoController.java
@@ -0,0 +1,22 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import model.Medico;
+
+public class ListaMedicoController {
+	
+	
+	@FXML
+	private void initialize() {
+		
+	}
+}
