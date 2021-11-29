b7243abf717ecd788ede1cc4a038275fd5328bcc
alisson <alisson@alisson-All-Series>
2021-11-29 09:25:45 -0300

    update view

--- Sistema Gerenciador Med/src/application/ListaProntuariosController.java
+++ Sistema Gerenciador Med/src/application/ListaProntuariosController.java
@@ -2,24 +2,44 @@ package application;
 
 import java.io.IOException;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import model.LinkList;
+import model.Paciente;
 
 public class ListaProntuariosController {
 	
 	@FXML
-	ListView<String> lista;
+	ListView<Paciente> lista;
 	
 	@FXML
 	private void initialize() {
-		ObservableList<String> items =FXCollections.observableArrayList("Alisson","fulano");
-		lista.setItems(items);
+		LinkList<Paciente> list = Main.main.getProntuarios();
+		list.resetIndex();
+		Paciente x =(Paciente) list.next();
+		if(x!=null) {
+			lista.getItems().add(x);
+		}
+		
+		while(list.getIndex()!=null) {
+			x =(Paciente) list.next();
+			if(x!=null) {
+				lista.getItems().add(x);
+			}
+		}
+	}
+	
+	@FXML
+	void add(ActionEvent event)throws IOException {
+		AnchorPane form = (AnchorPane)FXMLLoader.load(getClass().getResource("formProntuario.fxml"));
+		Scene sceneform = new Scene(form,800,600);
+		
+    	Main.aux.setScene(sceneform);
 	}
 	
 	@FXML
