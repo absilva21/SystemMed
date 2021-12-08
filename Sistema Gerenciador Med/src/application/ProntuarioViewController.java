04a423d8b2e3f55082b26997cdc19c81f7b86924
alisson <alisson@alisson-All-Series>
2021-12-08 16:24:55 -0300

    update prontuário

--- Sistema Gerenciador Med/src/application/ProntuarioViewController.java
+++ Sistema Gerenciador Med/src/application/ProntuarioViewController.java
@@ -3,8 +3,10 @@ package application;
 import java.io.IOException;
 import java.time.LocalDate;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.SubScene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
@@ -18,6 +20,14 @@ public class ProntuarioViewController {
 	
 	@FXML
 	Label name;
+	
+	@FXML
+	void volta(ActionEvent e) throws IOException {
+		AnchorPane form = (AnchorPane)FXMLLoader.load(getClass().getResource("agendaMed.fxml"));
+		Scene sceneform = new Scene(form,800,600);
+		         
+    	Main.aux.setScene(sceneform);
+	}
 	
 	@FXML
 	private void initialize() throws IOException {
